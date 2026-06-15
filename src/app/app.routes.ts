bb13c84cf6502621da28f2c9bc6cf4d9aa9f1958
rosado_c <rosado_c@f112.ub.lan>
2026-06-15 08:51:43 +0800

    Changes in Laboratory Activity 7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home'; 
+import { KantoRegion } from './kanto-region/kanto-region';
+import { JohtoRegion } from './johto-region/johto-region';
+import { HoennRegion } from './hoenn-region'; 
+import { LeaderInfo } from './leader-info/leader-info';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'kanto', component: KantoRegion },
+  { path: 'johto', component: JohtoRegion },
+  { path: 'hoenn', component: HoennRegion }, 
+  { path: 'leader-info', component: LeaderInfo },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
